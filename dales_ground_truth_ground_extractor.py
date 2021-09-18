890ccea195049adbf3e3c0291e738a5a8193ca58
Adi <adi.album1@gmail.com>
2021-09-18 15:56:42 +0300

    beautify dales_ground_truth_ground_extractor.py

--- dales_ground_truth_ground_extractor.py
+++ dales_ground_truth_ground_extractor.py
@@ -3,10 +3,11 @@ import argparse
 import laspy
 from dales_classes import DalesClasses
 
-if __name__ == "__main__":
+def parse_input_arguments():
     # Create the parser
     my_parser = argparse.ArgumentParser(description='Extract DALES dataset LAS file\'s ground truth ground points')
 
+    # add argparse argumnets
     my_parser.add_argument('-i', '--input', type=str, help='path to input LAS file',
                            default='dales_las/train/5080_54435.las')
     my_parser.add_argument('-o', '--output', type=str, help='path to output LAS file',
@@ -15,8 +16,13 @@ if __name__ == "__main__":
     # Execute the parse_args() method
     args = my_parser.parse_args()
 
-    input_las_path = args.input
-    output_las_path = args.output
+    input_las, output_las = args.input, args.output
+
+    return input_las, output_las
+
+
+if __name__ == "__main__":
+    input_las_path, output_las_path = parse_input_arguments()
 
     las = laspy.read(input_las_path)
     las.points = las.points[las.classification == DalesClasses.GROUND.value]
